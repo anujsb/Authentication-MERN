234bc047b338330972c9cb14fd40bf5afa57e501
anuj <anujsbhuyar@gmail.com>
2024-02-01 09:28:15 +0530

    frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/pages/SingIn.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class SingIn extends Component {
+  render() {
+    return (
+      <div>SingIn</div>
+    )
+  }
+}
+
+export default SingIn
