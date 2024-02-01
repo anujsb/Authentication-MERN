4944f571e66c3c37e240cdc8e561dc30ea57de1f
anuj <anujsbhuyar@gmail.com>
2024-02-01 11:38:53 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SingIn from "./pages/SingIn";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/SingIn" element={<SingIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/Profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
