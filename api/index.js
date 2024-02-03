e836a1d3bfbc6a514949c25d0ca5929896224110
anuj <anujsbhuyar@gmail.com>
2024-02-03 16:19:22 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() =>{
+    console.log('connected to mongoDB...');
+})
+.catch((err) =>{
+    console.log(err);
+});
 
 const app = express();
 
 app.listen(3000, () =>{
     console.log("Server listening on port 3000...")
-});
+}); 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
