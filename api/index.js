7ff552569bb6cfb96181f50f50f372df960e4248
anuj <anujsbhuyar@gmail.com>
2024-09-27 18:57:06 +0530

    ?

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+
+// mongoose.connect("  ")
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log('server listning to 3000')
+app.listen(3000, () => {
+  console.log("server listning to 3000");
 });
