a142f22d06ead0fa93b9b5a1a9b632c6869a7778
anujsb <anujsbhuyar@gmail.com>
2024-10-09 19:09:40 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,10 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-// mongoose.connect("  ")
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("server listning to 3000");
 });
+// yo83E0qCXdzUqtam
+// notlasagna17
