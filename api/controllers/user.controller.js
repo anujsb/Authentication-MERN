5f2b616e99fd68a618084f20688227672e0e16ae
anujsb <anujsbhuyar@gmail.com>
2024-10-13 19:53:58 +0530

    create signup api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test = (req, res) => {
+  res.json({
+    message: "APi is working",
+  });
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -14,8 +16,13 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("server listning to 3000");
 });
+
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
 // yo83E0qCXdzUqtam
 // notlasagna17
